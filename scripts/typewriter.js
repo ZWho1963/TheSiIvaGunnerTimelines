28b387f9b254db54ab5726955840aee26b06d424
ZWho1963 <zwho1963@gmail.com>
2026-01-15 13:48:00 +1100

    Revert timeline changes

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -15,7 +15,26 @@ function profitttheme() {
 }
 
 let tl = gsap.timeline();
-tl.to(split.chars, {
+tl.to("#line1", { duration: 1, text: "HWAC Public Access Portal v2026.01", ease: "none" })
+	.to("#line2", { duration: 1, text: "[info] Establishing connection", ease: "none" })
+	.to("#dots1", { duration: 3, text: "...", ease: "none" })
+	.to("#line2a", { duration: 0.1, text: "OK!", ease: "none", delay: 1 })
+	.to("#line3", { duration: 1, text: "[info] Connecting to database archive", ease: "none", delay: 0.5 })
+	.to("#dots2", { duration: 3, text: "...", ease: "none" })
+	.to("#line3a", { duration: 0.1, text: "OK!", ease: "none", delay: 1 })
+	.to("#line4", { duration: 1, text: "[info] Initialising experience", ease: "none", delay: 0.5 })
+	.to("#dots3", { duration: 4, text: "....", ease: "none" })
+	.from(split.chars, {
+		x: 150,
+		opacity: 0,
+		duration: 0.7,
+		ease: "power4",
+		stagger: 0.04,
+		delay: 0.5
+	})
+	.call(profitttheme, null, "<")
+	.to("#terminal", {duration: 0.7, opacity: 0}, '<')
+	.to(split.chars, {
 		x: -150,
 		opacity: 0,
 		duration: 0.7,
